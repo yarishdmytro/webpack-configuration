9ab1ae8e43ed563cffa3dd01522a24e2d6a9ea5a
yarishdmytro <yarish97@ukr.net>
2020-11-10 21:39:13 +0200

    Task-2

---
+++ webpack.config.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ]
+})
